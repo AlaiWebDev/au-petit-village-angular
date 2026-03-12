8d3eefeb648ac81e60f8a40e597545b9bfd09f5a
Dubois Celine <celine.dubois17@free.fr>
2026-03-12 18:02:47 +0100

    Mettre en place le routing + Afficher le header et le footer sur toutes les pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Product } from './product/product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'about',
+        component: About
+    },
+    {
+        path: 'product/:id',
+        component: Product
+    },
+];
