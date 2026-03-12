3a2fc8ef65974235915ba86d27d600f420fa05bb
Dubois Celine <celine.dubois17@free.fr>
2026-03-12 19:00:12 +0100

    Activer la route Product

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
+  standalone: true,
   imports: [],
   templateUrl: './product.html',
   styleUrl: './product.css',
 })
 export class Product {
-
+  productId: string|null=null;
+  constructor(private route: ActivatedRoute) {
+    this.productId = this.route.snapshot.paramMap.get("Id");
+    console.log("ID du produit:",this.productId);
+  }
 }
